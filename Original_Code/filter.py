9b9bf370b1b3ae96463ee51a31b6c8d304f70647
yedianyang <jimjguo@live.com>
2022-12-09 02:46:37 +0800

    Glitching movement version, and the moving target section is not start yet

--- Original_Code/filter.py
+++ Original_Code/filter.py
@@ -1,12 +1,11 @@
 class Filter:
     def __init__(self):
-        self.data_filter = [0, 0, 0, 0]
+        self.data_filter = [0, 0, 0, 0, 0, 0, 0, 0]
 
     def average_filter(self, signalIn):
         self.data_filter.pop(len(self.data_filter)-1)
         self.data_filter.insert(0, signalIn)
         return sum(self.data_filter)/len(self.data_filter)
-
 
 
 def mapping(value, leftMin, leftMax, rightMin, rightMax):
