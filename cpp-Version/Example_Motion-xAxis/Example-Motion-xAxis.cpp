9de6c921d3d24bac8014cb68f01456d100cb7a45
Jingxi Guo <jimjguo@live.com>
2025-04-22 13:35:53 -0400

    Update Example-Motion-xAxis.cpp
    quick test

--- cpp-Version/Example_Motion-xAxis/Example-Motion-xAxis.cpp
+++ cpp-Version/Example_Motion-xAxis/Example-Motion-xAxis.cpp
@@ -129,7 +129,7 @@ void motorControlThread(IPort& myPort) {
 		motorThread.detach();
 		*/
 		
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		//std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
 }
 
